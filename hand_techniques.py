99ad3e6b750fc4e8cd5a378f2918b45d1f480fb7
Joshua Lee <jleey2k@gmail.com>
2019-12-08 12:57:01 -0500

    Update hand_techniques.py

--- hand_techniques.py
+++ hand_techniques.py
@@ -35,13 +35,41 @@ def arm_angle(keypoints):
     ''' 
     output: [left arm angle, right arm angle], horizontally given assuming performer is facing the front
     '''
-    lwrist = keypoints[BODY_25["LWrist"]*3], keypoints[BODY_25["LWrist"]*3+1]
-    lshoulder = keypoints[BODY_25["LShoulder"]*3], keypoints[BODY_25["LShoulder"]*3+1]
-    lhip = keypoints[BODY_25["LHip"]*3], keypoints[BODY_25["LHip"]*3+1]
-    return math.acos()
+    lwrist = np.array([keypoints[BODY_25["LWrist"]*3], keypoints[BODY_25["LWrist"]*3+1]])
+    lshoulder = np.array([keypoints[BODY_25["LShoulder"]*3], keypoints[BODY_25["LShoulder"]*3+1]])
+    lhip = np.array([keypoints[BODY_25["LHip"]*3], keypoints[BODY_25["LHip"]*3+1]])
+    larm = lwrist-lshouder
+    lbody = lhip-lshoulder
+    left_arm_angle = np.arccos(np.clip(np.dot(larm/np.linalg.norm(larm),lbody/np.linalg.norm(lbody)),-1.0,1.0))
+
+    rwrist = np.array([keypoints[BODY_25["RWrist"]*3], keypoints[BODY_25["RWrist"]*3+1]])
+    rshoulder = np.array([keypoints[BODY_25["RShoulder"]*3], keypoints[BODY_25["RShoulder"]*3+1]])
+    rhip = np.array([keypoints[BODY_25["RHip"]*3], keypoints[BODY_25["RHip"]*3+1]])
+    rarm = rwrist-rshouder
+    rbody = rhip-rshoulder
+    right_arm_angle = np.arccos(np.clip(np.dot(rarm/np.linalg.norm(rarm),rbody/np.linalg.norm(rbody)),-1.0,1.0))
+
+    return [left_arm_angle,right_arm_angle]
 
 def leg_angle(keypoints):
+    ''' 
+    output: [left leg angle, right leg angle], horizontally given assuming performer is facing the front
+    '''
+    lankle = np.array([keypoints[BODY_25["LAnkle"]*3], keypoints[BODY_25["LAnkle"]*3+1]])
+    lshoulder = np.array([keypoints[BODY_25["LShoulder"]*3], keypoints[BODY_25["LShoulder"]*3+1]])
+    lhip = np.array([keypoints[BODY_25["LHip"]*3], keypoints[BODY_25["LHip"]*3+1]])
+    lleg = lankle-lhip
+    lbody = lshoulder-lhip
+    left_left_angle = np.arccos(np.clip(np.dot(lleg/np.linalg.norm(lleg),lbody/np.linalg.norm(lbody)),-1.0,1.0))
 
+    rwrist = np.array([keypoints[BODY_25["RWrist"]*3], keypoints[BODY_25["RWrist"]*3+1]])
+    rshoulder = np.array([keypoints[BODY_25["RShoulder"]*3], keypoints[BODY_25["RShoulder"]*3+1]])
+    rhip = np.array([keypoints[BODY_25["RHip"]*3], keypoints[BODY_25["RHip"]*3+1]])
+    rleg = rankle-rhip
+    rbody = rshoulder-rhip
+    right_leg_angle = np.arccos(np.clip(np.dot(rleg/np.linalg.norm(rleg),rbody/np.linalg.norm(rbody)),-1.0,1.0))
+
+    return [left_leg_angle,right_leg_angle]
 
 # STRIKES
 def is_valid_middlePunch(p, dir=None):
